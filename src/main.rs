986ac2e004d0c61001e4bd2ccef776cf2a1efece
Peat Bakke <peat@peat.org>
2022-10-28 22:32:12 -0700

    comparison benchmarking at about 250k pixels per ms

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
+use image::{DynamicImage, GenericImageView, Rgba};
+use std::time::Instant;
+
 mod main_window;
 
 // use image::{DynamicImage, Pixel, Rgba, RgbaImage};
 
 fn main() {
+    let source = ImageSet::open("teapot.jpg").unwrap();
+
+    let start_time = Instant::now();
+    let deltas = source.deltas();
+    let stop_time = Instant::now();
+    let elapsed = stop_time - start_time;
+
+    let comparisons = source.source.pixels().count() * 2;
+
+    println!("{:?}", deltas);
+    println!("Comparisons: {}", comparisons);
+    println!("Elapsed: {:?}", elapsed);
+    println!("Rate: {}/ms", (comparisons as u128) / elapsed.as_millis());
+
     // UI run loop; doesn't exit.
-    main_window::run();
+    // main_window::run();
 }
 
 /*
@@ -22,7 +39,95 @@ fn main() {
 */
 
 #[derive(Debug)]
-struct Region {
+pub struct ImageDeltas {
+    candidate: usize,
+    current: usize,
+}
+
+impl ImageDeltas {
+    pub fn new() -> Self {
+        Self {
+            candidate: 0,
+            current: 0,
+        }
+    }
+}
+
+pub struct ImageSet {
+    source: DynamicImage,
+    candidate: DynamicImage,
+    current: DynamicImage,
+}
+
+impl ImageSet {
+    pub fn open(path: &str) -> Result<Self, String> {
+        let source = image::open(path).map_err(|e| format!("{}", e))?;
+        let candidate = DynamicImage::new_rgba8(source.width(), source.height());
+        let current = candidate.clone();
+
+        Ok(Self {
+            source,
+            candidate,
+            current,
+        })
+    }
+
+    pub fn width(&self) -> u32 {
+        self.source.width()
+    }
+
+    pub fn height(&self) -> u32 {
+        self.source.height()
+    }
+
+    pub fn region(&self, x: u32, y: u32, width: u32, height: u32) -> ImageSet {
+        let source = self.source.crop_imm(x, y, width, height);
+        let candidate = self.candidate.crop_imm(x, y, width, height);
+        let current = self.current.crop_imm(x, y, width, height);
+
+        Self {
+            source,
+            candidate,
+            current,
+        }
+    }
+
+    fn channel_delta(a: u8, b: u8) -> u8 {
+        if a >= b {
+            a - b
+        } else {
+            b - a
+        }
+    }
+
+    fn delta(a: Rgba<u8>, b: Rgba<u8>) -> usize {
+        // convert to RGB without the alpha channel so that we can compare
+        // the visible values
+
+        let mut delta = 0;
+
+        delta += Self::channel_delta(a[0], b[0]) as usize;
+        delta += Self::channel_delta(a[1], b[1]) as usize;
+        delta += Self::channel_delta(a[2], b[2]) as usize;
+        delta
+    }
+
+    pub fn deltas(&self) -> ImageDeltas {
+        let mut deltas = ImageDeltas::new();
+
+        deltas.candidate = std::iter::zip(self.source.pixels(), self.candidate.pixels())
+            .map(|(a, b)| Self::delta(a.2, b.2))
+            .sum();
+        deltas.current = std::iter::zip(self.source.pixels(), self.current.pixels())
+            .map(|(a, b)| Self::delta(a.2, b.2))
+            .sum();
+
+        deltas
+    }
+}
+
+#[derive(Debug)]
+pub struct Region {
     start_x: usize,
     start_y: usize,
     end_x: usize,
@@ -61,8 +166,8 @@ impl Region {
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct Point {
-    x: usize,
-    y: usize,
+    pub x: usize,
+    pub y: usize,
 }
 
 impl Point {
@@ -86,8 +191,6 @@ impl Iterator for Region {
             x: self.index_x,
             y: self.index_y,
         };
-
-        // adjust the indexes ...
 
         // check to see if we head to the next row
         if self.index_x == self.end_x {
