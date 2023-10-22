4c281a83c33bfbf39b1445fa0221721e511a47d8
Peat Bakke <peat@peat.org>
2023-10-22 12:09:19 -0700

    Use VecDeque in RateMeter; adding tests

--- src/rate_meter.rs
+++ src/rate_meter.rs
@@ -1,21 +1,23 @@
+use std::collections::VecDeque;
+
 pub struct RateMeter {
     limit: usize,
-    samples: Vec<usize>,
+    samples: VecDeque<usize>,
 }
 
 impl RateMeter {
     pub fn new(limit: usize) -> Self {
         Self {
             limit,
-            samples: vec![],
+            samples: VecDeque::new(),
         }
     }
 
     pub fn sample(&mut self, value: usize) {
-        self.samples.push(value);
+        self.samples.push_back(value);
 
         if self.samples.len() > self.limit {
-            self.samples.remove(0);
+            self.samples.pop_front();
         }
     }
 
@@ -30,7 +32,7 @@ impl RateMeter {
     }
 
     pub fn reset(&mut self) {
-        self.samples = vec![];
+        self.samples = VecDeque::new();
     }
 
     /// Defaults false if there aren't enough samples
@@ -40,5 +42,50 @@ impl RateMeter {
         } else {
             false
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn limit_samples() {
+        let mut m = RateMeter::new(1);
+
+        m.sample(1);
+        assert_eq!(m.samples.len(), 1);
+
+        m.sample(1);
+        assert_eq!(m.samples.len(), 1);
+    }
+
+    #[test]
+    fn averages_correctly() {
+        let mut m = RateMeter::new(3);
+
+        m.sample(1);
+        m.sample(2);
+        m.sample(3);
+        assert_eq!(m.rate(), Some(2.0));
+
+        m.sample(4);
+        assert_eq!(m.rate(), Some(3.0));
+
+        m.sample(5);
+        assert_eq!(m.rate(), Some(4.0));
+    }
+
+    #[test]
+    fn only_reports_if_full() {
+        let mut m = RateMeter::new(3);
+        m.sample(1);
+        assert_eq!(m.rate(), None);
+
+        m.sample(2);
+        assert_eq!(m.rate(), None);
+
+        m.sample(3);
+        assert_eq!(m.rate(), Some(2.0));
     }
 }
