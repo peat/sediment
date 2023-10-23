3ab1dbb1daa4cd4ae7ee417962f71d72d12055b5
Peat Bakke <peat@peat.org>
2023-10-23 08:57:52 -0700

    Reference and preview now side by side and sized appropriately

--- src/gui.rs
+++ src/gui.rs
@@ -4,16 +4,21 @@ use std::thread;
 
 use crate::builder::{Builder, BuilderUpdate, Stats};
 use crate::BuildConfig;
+use crate::Canvas;
 use eframe::{egui, epaint::ColorImage, App, CreationContext, NativeOptions};
 use image::DynamicImage;
 
 static PREVIEW_TEXTURE_ID: &str = "preview-image";
+static REFERENCE_TEXTURE_ID: &str = "reference-image";
 
 pub fn run(build_config: BuildConfig) {
     let (builder_tx, builder_update_rx) = channel();
 
+    let builder_config = build_config.clone();
+    let gui_config = build_config.clone();
+
     thread::spawn(move || {
-        let mut builder = Builder::new(builder_tx, build_config);
+        let mut builder = Builder::new(builder_tx, builder_config);
         builder.run();
     });
 
@@ -25,43 +30,50 @@ pub fn run(build_config: BuildConfig) {
     let _ = eframe::run_native(
         "Sediment",
         options,
-        Box::new(|cc| Box::new(SedimentApp::new(cc, builder_update_rx))),
+        Box::new(|cc| Box::new(SedimentApp::new(cc, gui_config, builder_update_rx))),
     );
 }
 
 pub struct SedimentApp {
     rx: Receiver<BuilderUpdate>,
+    reference_texture: Option<egui::TextureHandle>,
     preview_texture: Option<egui::TextureHandle>,
     preview_image: ColorImage,
     stats_line: String,
+    config: BuildConfig,
 }
 
 impl SedimentApp {
-    pub fn new(_creation_context: &CreationContext<'_>, rx: Receiver<BuilderUpdate>) -> Self {
+    pub fn new(
+        _creation_context: &CreationContext<'_>,
+        config: BuildConfig,
+        rx: Receiver<BuilderUpdate>,
+    ) -> Self {
         Self {
             rx,
+            reference_texture: None,
             preview_image: egui::ColorImage::example(),
             preview_texture: None,
             stats_line: String::new(),
+            config,
         }
     }
 
-    pub fn window_title(&self) -> String {
-        "Sediment".to_owned()
+    fn dynamic_image_to_color_image(dynamic_image: DynamicImage) -> ColorImage {
+        let rgba8 = dynamic_image.to_rgba8();
+        let pixels = rgba8.as_flat_samples();
+        ColorImage::from_rgba_unmultiplied(
+            [dynamic_image.width() as _, dynamic_image.height() as _],
+            pixels.as_slice(),
+        )
     }
 
-    pub fn handle_new_preview(&mut self, new_preview: DynamicImage) {
-        let rgba8 = new_preview.to_rgba8();
-        let pixels = rgba8.as_flat_samples();
-        let egui_img = ColorImage::from_rgba_unmultiplied(
-            [new_preview.width() as _, new_preview.height() as _],
-            pixels.as_slice(),
-        );
-        self.preview_image = egui_img;
+    fn update_preview(&mut self, new_preview: DynamicImage) {
+        self.preview_image = Self::dynamic_image_to_color_image(new_preview);
         self.preview_texture = None; // clear the texture so that update() knows to rebuild it.
     }
 
-    pub fn update_status(&mut self, stats: Stats) {
+    fn update_status(&mut self, stats: Stats) {
         self.stats_line = format!(
             "Attempts: {}, Shapes: {}, Current Radius: {}, Elapsed: {:.2}s",
             stats.total_attempts,
@@ -71,27 +83,50 @@ impl SedimentApp {
         );
     }
 
-    pub fn fit_image_to_window(
-        image_width: f32,
-        image_height: f32,
-        window_width: f32,
-        window_height: f32,
-    ) -> (f32, f32) {
-        let aspect_ratio = image_width / image_height;
-        let mut new_width = window_width;
+    fn maximize_dimensions(original: egui::Vec2, available: egui::Vec2) -> egui::Vec2 {
+        let aspect_ratio = original.x / original.y;
+        let mut new_width = available.x;
         let mut new_height = new_width / aspect_ratio;
 
-        if new_height > window_height {
-            new_height = window_height;
+        if new_height > available.y {
+            new_height = available.y;
             new_width = new_height * aspect_ratio;
         }
 
-        (new_width, new_height)
+        egui::Vec2::new(new_width, new_height)
+    }
+
+    fn scale_texture_to_fit(
+        ui: &egui::Ui,
+        texture: &egui::TextureHandle,
+    ) -> (egui::TextureId, egui::Vec2) {
+        let new_dimensions = Self::maximize_dimensions(texture.size_vec2(), ui.available_size());
+
+        (texture.id(), new_dimensions)
+    }
+
+    fn preview_texture_handle(&mut self, ui: &mut egui::Ui) -> &egui::TextureHandle {
+        self.preview_texture.get_or_insert_with(|| {
+            ui.ctx().load_texture(
+                PREVIEW_TEXTURE_ID,
+                self.preview_image.clone(),
+                Default::default(),
+            )
+        })
+    }
+
+    fn reference_texture_handle(&mut self, ui: &mut egui::Ui) -> &egui::TextureHandle {
+        self.reference_texture.get_or_insert_with(|| {
+            let reference_image = Canvas::open(&self.config.input).unwrap();
+            let reference_texture = Self::dynamic_image_to_color_image(reference_image.img);
+            ui.ctx()
+                .load_texture(REFERENCE_TEXTURE_ID, reference_texture, Default::default())
+        })
     }
 }
 
 impl App for SedimentApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         ctx.request_repaint(); // continuous repainting
 
         if ctx.input(|i| i.key_released(egui::Key::Escape)) {
@@ -101,44 +136,48 @@ impl App for SedimentApp {
         // handle any messages that may have come in from the builder
         if let Ok(input) = self.rx.try_recv() {
             match input {
-                BuilderUpdate::Preview(new_preview) => self.handle_new_preview(new_preview),
+                BuilderUpdate::Preview(new_preview) => self.update_preview(new_preview),
                 BuilderUpdate::Stats(s) => self.update_status(s),
             }
         }
 
-        // top panel
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            ui.heading(&self.window_title());
-        });
+        // window is split into four panels:
+        // - left panel is the reference image
+        // - middle panel is the generated image
+        // - bottom panel is the status line
 
-        // bottom panel
-        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+        let image_margin_px = 10.0;
+        let image_margin = egui::Margin::same(image_margin_px);
+        let panel_frame = egui::Frame::none().outer_margin(image_margin);
+
+        // calculate panel widths (50% of the window)
+        let window_dimensions = frame.info().window_info.size;
+        let panel_width = (window_dimensions.x / 2.0) - (image_margin_px * 2.0);
+
+        // bottom panel status line
+        egui::TopBottomPanel::bottom("stats_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.spacing_mut().item_spacing.y = 10.0;
                 ui.label(&self.stats_line);
             });
         });
 
-        // main content
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let texture: &egui::TextureHandle = self.preview_texture.get_or_insert_with(|| {
-                ui.ctx().load_texture(
-                    PREVIEW_TEXTURE_ID,
-                    self.preview_image.clone(),
-                    Default::default(),
-                )
+        egui::SidePanel::left("reference_panel")
+            .exact_width(panel_width)
+            .resizable(false)
+            .show_separator_line(true)
+            .frame(panel_frame)
+            .show(ctx, |ui| {
+                let texture = self.reference_texture_handle(ui);
+                ui.image(Self::scale_texture_to_fit(ui, texture));
             });
 
-            // scale the image to fit the available space
-            let ui_size = ui.available_size();
-            let image_size = texture.size_vec2();
-
-            let (new_width, new_height) =
-                Self::fit_image_to_window(image_size.x, image_size.y, ui_size.x, ui_size.y);
-
-            let new_dimensions = egui::Vec2::new(new_width, new_height);
-
-            ui.image((texture.id(), new_dimensions));
-        });
+        // generated image preview
+        egui::CentralPanel::default()
+            .frame(panel_frame)
+            .show(ctx, |ui| {
+                let texture = self.preview_texture_handle(ui);
+                ui.image(Self::scale_texture_to_fit(ui, texture));
+            });
     }
 }
